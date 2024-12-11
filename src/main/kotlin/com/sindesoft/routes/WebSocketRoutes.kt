75cff0a402833afc00e164599d9302bdbb300521
Valentin Valencia Valencia <valentinv3v7@gmail.com>
2024-12-11 12:44:36 -0500

    added infinitePing

--- src/main/kotlin/com/sindesoft/routes/WebSocketRoutes.kt
+++ src/main/kotlin/com/sindesoft/routes/WebSocketRoutes.kt
@@ -1,8 +1,12 @@
 package com.sindesoft.routes
 
+import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 fun Route.socketRouting(){
 
@@ -20,6 +24,36 @@ fun Route.socketRouting(){
                 }
             }
         }
+
+        webSocket("/infinitePing"){
+            try {
+                // Start a coroutine to send pings periodically
+                val pingJob = launch {
+                    while (true) {
+                        delay(3_000) // 10 seconds delay
+                        send("ping")
+                    }
+                }
+
+                // Handle incoming messages
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    if (receivedText.equals("bye", ignoreCase = true)) {
+                        close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+                        break
+                    } else {
+                        send("Received: $receivedText")
+                    }
+                }
+
+                // Cancel the ping job if the loop ends
+                pingJob.cancelAndJoin()
+            } catch (e: Exception) {
+                application.log.error("WebSocket error: ${e.message}", e)
+            }
+        }
+
     }
 
 }
